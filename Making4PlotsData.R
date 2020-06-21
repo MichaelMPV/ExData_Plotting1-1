97c7e803ccb856635f1f46e0e9271a7519848d12
damiangoli <65316454+damiangoli@users.noreply.github.com>
2020-06-21 11:17:22 -0400

    Add files via upload

---
+++ Making4PlotsData.R
@@ -0,0 +1,24 @@
+## file loading and assigning classes
+fileload <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", 
+    colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+fileload$Date <- as.Date(fileload$Date, "%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007 and remove incomplate observation
+fileload <- subset(fileload,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+fileload <- fileload[complete.cases(fileload),]
+
+## Combine Date and Time column and assigning name
+dateTime <- paste(fileload$Date, fileload$Time)
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column and adding DateTime column
+fileload <- fileload[ ,!(names(fileload) %in% c("Date","Time"))]
+fileload <- cbind(dateTime, fileload)
+
+## Format dateTime Column
+fileload$dateTime <- as.POSIXct(dateTime)
+
+
+
+
+
---
+++ plot1.R
@@ -0,0 +1,7 @@
+## Plot 1 file
+hist(fileload$Global_active_power, 
+     main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
